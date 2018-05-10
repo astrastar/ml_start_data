18266c0cb7978e8ea649f22906dc1488a0e5500f
astrastar <37809311+astrastar@users.noreply.github.com>
2018-05-10 11:09:41 -0400

    multi request

--- get_price.py
+++ get_price.py
@@ -2,7 +2,8 @@ import requests
 
 
 def get_price(limit):
-    url = f'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit={limit}'
+    time = 000000
+    url = f'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit={limit}&toTs={time}'
     response = requests.get(url)
     return response.json().get('Data')
 
