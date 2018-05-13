9fda845f90eee186c59c032c5540ad0c8d154206
astrastar <37809311+astrastar@users.noreply.github.com>
2018-05-13 11:08:00 -0400

    works fot multi requests and limit more then 1

--- req_cls.py
+++ req_cls.py
@@ -5,7 +5,6 @@ class RequestPrice:
     Limit - number of candels in response, num_request - number of request with given limit
     """
     url1 = 'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit='
-    # final_response = []
 
     def __init__(self, limit, num_request):
         self.limit = limit - 1
@@ -19,8 +18,8 @@ class RequestPrice:
     def other_req(self):
         request_list = []
         time_to = self.first_req()[0].get('time')
-        for req in range(self.num_request):
-            local_url = f'{self.url1}{self.limit}&toTs={time_to}'
+        for req in range((self.num_request) - 1):
+            local_url = f'{self.url1}{self.limit + 1}&toTs={time_to}'
             response = requests.get(local_url)
             response_json = response.json().get('Data')
             request_list.extend(response_json[0:-1])
@@ -35,8 +34,12 @@ class RequestPrice:
         return final_res
 
 
-x = RequestPrice(5, 10)
-print(x.final_response)
+x = RequestPrice(2, 2)
+print(len(x.final_response))
+print(x.first_req())
+
+
+
 
 
 
