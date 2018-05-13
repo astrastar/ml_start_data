4bfdc298e09d16f933abbdf10548125ef7b12a0d
astrastar <37809311+astrastar@users.noreply.github.com>
2018-05-13 04:30:30 -0400

    final_request working but not correct

--- req_cls.py
+++ req_cls.py
@@ -5,10 +5,10 @@ class RequestPrice:
     Limit - number of candels in response, num_request - number of request with given limit
     """
     url1 = 'https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit='
-    final_response = []
+    # final_response = []
 
     def __init__(self, limit, num_request):
-        self.limit = limit
+        self.limit = limit - 1
         self.num_request = num_request
 
     def first_req(self):
@@ -22,7 +22,24 @@ class RequestPrice:
         for req in range(self.num_request):
             local_url = f'{self.url1}{self.limit}&toTs={time_to}'
             response = requests.get(local_url)
+            response_json = response.json().get('Data')
+            request_list.extend(response_json[0:-1])
+            time_to = response_json[0].get('time')
+        return request_list
 
-#
-# x = RequestPrice(10, 1)
-# print(x.final_response)
+    @property
+    def final_response(self):
+        final_res = []
+        final_res.extend(self.first_req())
+        final_res.extend(self.other_req())
+        return final_res
+
+
+x = RequestPrice(5, 10)
+print(x.final_response)
+
+
+
+
+
+
